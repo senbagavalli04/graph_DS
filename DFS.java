76a90559993a6f4774c8d3d735811dc7c6eae5ea
Senbagavalli N <142738030+senbagavalli04@users.noreply.github.com>
2025-02-03 09:52:45 +0530

    dfs

---
+++ DFS.java
@@ -0,0 +1,55 @@
+// Link : https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
+       ArrayList<Integer> ans = new ArrayList<>();
+       int[] vis = new int[adj.size()];
+       dfs(adj,0,ans,vis);
+       return ans;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> adj, int src,ArrayList<Integer> ans, int[] vis){
+        ans.add(src);
+        vis[src] = 1;
+        ArrayList<Integer> al = adj.get(src);
+        for(int i : al){
+            if(vis[i] == 0) dfs(adj,i,ans,vis);
+        }
+    }
+}
